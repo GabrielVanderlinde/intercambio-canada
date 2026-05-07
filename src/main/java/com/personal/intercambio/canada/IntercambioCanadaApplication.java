d57ad76f3bff5c4a0dfe955ae031f98953794ee3
GabrielVanderlinde-Qualle <gabriel.vanderlinde@quallecontrol.com.br>
2026-05-07 13:54:01 -0300

    first commit - base architecture

---
+++ src/main/java/com/personal/intercambio/canada/IntercambioCanadaApplication.java
@@ -0,0 +1,13 @@
+package com.personal.intercambio.canada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntercambioCanadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntercambioCanadaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personal/intercambio/canada/IntercambioCanadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.intercambio.canada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntercambioCanadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
